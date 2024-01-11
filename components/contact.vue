04a07aea2029d64aea36c7084efd59a71a2b21a1
ishaansp <ishaan_sharma@softprodigy.com>
2024-01-11 22:27:51 +0530

    modal added

---
+++ components/contact.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="modal fade" id="contactModal" tabindex="-1" role="dialog" aria-labelledby="contactModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="contactModalLabel">Contact Form</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <!-- Your contact form goes here -->
+          <form @submit.prevent="submitForm">
+            <!-- Form fields go here -->
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" class="form-control" id="name" v-model="formData.name" required>
+            </div>
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="email" class="form-control" id="email" v-model="formData.email" required>
+            </div>
+            <!-- Add more form fields as needed -->
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        // Add more form fields as needed
+      },
+    };
+  },
+  methods: {
+    submitForm() {
+      // Handle form submission logic here
+      console.log('Form submitted', this.formData);
+      // You can use a library like axios to send the form data to your server
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add your custom styles here */
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    	<button
+						v-b-modal.modal-2
+					>open</button>
+    <b-modal id="modal-2" no-close-on-backdrop centered>
+<div id="icontactSignupFormWrapper473"><script type="text/javascript" async src="https://app.icontact.com/icp/core/mycontacts/signup/designer/form/automatic?id=473&cid=1912706&lid=7659&divid=icontactSignupFormWrapper473"></script></div>
+
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import Contact from "../components/contact.vue";
+
+export default {
+  components: {
+    Contact,
+  },
+};
+</script>
