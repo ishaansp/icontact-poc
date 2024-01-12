487ade6c2844b7f6d97c8703e604e4f3ff29f52a
ishaansp <ishaan_sharma@softprodigy.com>
2024-01-12 13:01:29 +0530

    testing icontact

--- pages/test.vue
+++ pages/test.vue
@@ -5,8 +5,11 @@
     	<button
 						v-b-modal.modal-2
 					>open</button>
-    <b-modal id="modal-2" no-close-on-backdrop centered>
-<div id="icontactSignupFormWrapper473"><script type="text/javascript" async src="https://app.icontact.com/icp/core/mycontacts/signup/designer/form/automatic?id=473&cid=1912706&lid=7659&divid=icontactSignupFormWrapper473"></script></div>
+    <b-modal @shown="showForm" id="modal-2" no-close-on-backdrop centered>
+      <div>
+      <div> loading </div>
+      <div id="mycustomform"> </div>
+      </div>
 
     </b-modal>
   </div>
@@ -19,5 +22,26 @@ export default {
   components: {
     Contact,
   },
+  methods:{
+    showForm(){
+      console.log('form')
+      var iDiv = document.createElement('div');
+iDiv.id = 'icontactSignupFormWrapper473';
+var a=document.getElementById("mycustomform");
+a.appendChild(iDiv)
+console.log("a",a)
+// document.getElementsById('mycustomform').appendChild(iDiv);
+
+const script = document.createElement('script')
+		script.type = 'text/javascript'
+		script.src = "https://app.icontact.com/icp/core/mycontacts/signup/designer/form/automatic?id=473&cid=1912706&lid=7659&divid=icontactSignupFormWrapper473"
+		script.async = true
+		script.defer = true
+
+		this.scriptele = document.body.appendChild(script)
+
+
+    }
+  }
 };
 </script>
